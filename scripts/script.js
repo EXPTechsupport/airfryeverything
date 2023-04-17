b8fcabdf97e62192a2b5c4af39936d623bded022
Andrew Wilks <112723526+andrewwilks295@users.noreply.github.com>
2023-04-17 15:48:58 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
+/*
+    Student Name: Andrew Wilks
+    File Name: script.js
+    Date: April 10, 2023
+*/
 
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("h-menu-links");
+    var logo = document.getElementById("logo-title");
+    if (menu.style.display === "block" && logo.style.display === "none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
