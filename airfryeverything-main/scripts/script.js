0bfb7a1fc48e1d2d2a2bd953c836d6a00f4dc3ce
esando17 <123141546+esando17@users.noreply.github.com>
2023-04-24 13:47:30 -0600

    Update script.js
    spinpage function is used to move text in contact page

--- airfryeverything-main/scripts/script.js
+++ airfryeverything-main/scripts/script.js
@@ -16,3 +16,24 @@ function hamburger() {
         logo.style.display = "none";
     }
 }
+//When the DOM is ready it will run the "spinpage" funtion
+$( document ).ready(function() {
+    spinpage()
+});
+
+//This is the spinpage funtion
+function spinpage(){
+    var spin = 0;
+//setInterval funtion calls an annomonus funtion which does all the "work"
+//"#welcome" calls the div id "welcome"
+    var name = setInterval(function () {
+        $("#welcome").css({'transform' : 'rotate('+ spin +'deg)'});
+        spin += 15;
+        if(spin == 360) {
+            clearInterval(name);
+            spin==0;
+            $("#welcome").css({'transform' : 'rotate('+ spin +'deg)'});
+        }   
+    }, 250);
+
+}
